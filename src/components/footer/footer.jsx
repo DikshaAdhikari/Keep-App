1fd2e21f99e84460cc5c340620a720a5d821f324
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2021-01-01 08:35:35 +0530

    footer-update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import "./footer.css";
+
 const Footer = () => {
     return <>
-        <footer>coded by <a href="https://github.com/DikshaAdhikari">Diksha Adhikari</a> </footer>
+        <footer>
+            coded by <a href="https://github.com/DikshaAdhikari">Diksha Adhikari</a>
+         </footer>
     </>;
 };
 
